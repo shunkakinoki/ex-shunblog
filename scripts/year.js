433ad3e11b0c2117035223767c71acbb28932d41
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-18 11:37:25 +0900

    added year section

---
+++ scripts/year.js
@@ -0,0 +1,23 @@
+const { resolve } = require("path");
+const { existsSync, lstatSync, readdirSync } = require("fs");
+
+function readYearFileList() {
+  const fileList = [];
+  const dirPath = resolve(`./docs/year/`);
+  const isDir = existsSync(dirPath) && lstatSync(dirPath).isDirectory();
+  if (!isDir) {
+    return fileList;
+  }
+
+  const files = readdirSync(dirPath);
+  files.forEach((item) => {
+    const currentFile = item.slice(0, 4);
+    fileList.push([
+      `/year/${currentFile}`,
+      `Year ${currentFile}`,
+    ]);
+  });
+  return fileList.reverse();
+}
+
+module.exports = readYearFileList;
